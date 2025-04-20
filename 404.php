f1f74f658f832ff9468a55f6450792ac69566939
Rahul Pandit <92500570+rahulpandit2@users.noreply.github.com>
2025-04-20 10:49:02 +0530

    bug fix: removed double navigation from 404.php

--- 404.php
+++ 404.php
@@ -39,27 +39,6 @@ require_once 'partials/header.php';
     </style>
 </head>
 <body class="bg-light">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">TradersHub Automated Trading</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
 
     <div class="container error-container">
         <div class="lottie-container">
